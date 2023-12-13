fee0af8b259e3927c3d7a33a14f314075518d3e0
eR.Lang <328590885@qq.com>
2023-12-13 08:45:22 +0800

    Bugfix: modify the unit of the elapsed time

--- examples/src/03-01.zig
+++ examples/src/03-01.zig
@@ -15,6 +15,6 @@ pub fn main() !void {
     const elapsed_ns: f64 = @floatFromInt(now.since(start));
 
     print("Time elapsed in expensive_function() is: {d:.3}ms", .{
-        elapsed_ns / time.ns_per_s,
+        elapsed_ns / time.ns_per_ms,
     });
 }
