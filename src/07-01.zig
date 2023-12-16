5369faf171f78c58dd5895e3c0f7fbc46fff1429
byte911 <92360792+byte911@users.noreply.github.com>
2023-12-16 17:09:11 +0800

    Add 07-01 Concurrency (#8)
    * Add 07-01 Explicit Threads
    
    * Update book-src/07-01-spawn.md
    
    ---------
    
    Co-authored-by: eR.Lang <328590885@qq.com>
    Co-authored-by: Jiacai Liu <dev@liujiacai.net>

---
+++ src/07-01.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var arr: [4]i32 = .{ 1, 25, -4, 10 };
+    var max_value: i32 = undefined;
+    try findMax(&max_value, &arr);
+    std.debug.assert(max_value == 25);
+}
+
+fn findMax(max_value: *i32, values: []i32) !void {
+    const THRESHOLD: usize = 2;
+
+    if (values.len <= THRESHOLD) {
+        var res = values[0];
+        for (values) |it| {
+            res = @max(res, it);
+        }
+        max_value.* = res;
+        return;
+    }
+
+    const mid = values.len / 2;
+    const left = values[0..mid];
+    const right = values[mid..];
+
+    var v1: i32 = undefined;
+    const t1 = try std.Thread.spawn(.{}, findMax, .{ &v1, left });
+    t1.join();
+
+    var v2: i32 = undefined;
+    const t2 = try std.Thread.spawn(.{}, findMax, .{ &v2, right });
+    t2.join();
+
+    max_value.* = @max(v1, v2);
+}
