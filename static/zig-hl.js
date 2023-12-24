7057bf88bd9f1e26bc50026e82e9b532d5e6e8b6
jiacai2050 <dev@liujiacai.net>
2023-12-24 20:49:01 +0800

    remove index.hbs

--- static/zig-hl.js
+++ static/zig-hl.js
@@ -4,7 +4,7 @@ const zigLanguageSupport = (hljs) => {
     aliases: ["zig"],
     keywords: {
       keyword:
-        "unreachable continue errdefer suspend return resume cancel break catch async await defer asm try " +
+      "unreachable continue errdefer suspend return resume cancel break catch async await defer asm try " +
         "threadlocal linksection allowzero stdcallcc volatile comptime noalias nakedcc inline export packed extern align const pub var " +
         "struct union error enum while for switch orelse else and if or usingnamespace test fn",
       type: "comptime_float comptime_int c_longdouble c_ulonglong c_longlong c_voidi8 noreturn c_ushort anyerror promise c_short c_ulong c_uint c_long isize c_int usize void f128 i128 type bool u128 u16 f64 f32 u64 i16 f16 i32 u32 i64 u8 i0 u0",
@@ -51,3 +51,11 @@ const zigLanguageSupport = (hljs) => {
     ],
   };
 };
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  if (typeof hljs !== 'undefined') {
+    console.log("register zig support");
+    hljs.registerLanguage("zig", zigLanguageSupport);
+    hljs.initHighlighting();
+  }
+});
