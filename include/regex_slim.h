41e6de8056afa6762f9736353bc0da1ea11da9ec
jiacai2050 <dev@liujiacai.net>
2023-12-30 21:22:59 +0800

    add comments

--- include/regex_slim.h
+++ include/regex_slim.h
@@ -1,3 +1,6 @@
+// Workaround for using regex in Zig.
+// https://www.openmymind.net/Regular-Expressions-in-Zig/
+// https://stackoverflow.com/questions/73086494/how-to-allocate-a-struct-of-incomplete-type-in-zig
 #include <regex.h>
 #include <stdalign.h>
 
