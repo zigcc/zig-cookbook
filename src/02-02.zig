f38fe68e2d9026b226ae9c560cd2eecf70725826
jiacai2050 <dev@liujiacai.net>
2023-12-31 22:43:40 +0800

    refactor: simplify pbkdf2

--- src/02-02.zig
+++ src/02-02.zig
@@ -4,19 +4,34 @@ const crypto = std.crypto;
 const HmacSha256 = crypto.auth.hmac.sha2.HmacSha256;
 
 pub fn main() !void {
-    var prng = std.rand.DefaultPrng.init(@intCast(std.time.timestamp()));
-    const rand = prng.random();
-
-    var salt: [64]u8 = undefined;
-    rand.bytes(&salt);
-
+    const salt = [_]u8{ 'a', 'b', 'c' };
     const password = "Guess Me If You Can!";
-    const rounds = 100_000;
-    const derived_key_len = 64;
+    const rounds = 1_000;
 
-    var derived_key: [derived_key_len]u8 = undefined;
+    // Usually 16 or 32 bytes
+    var derived_key: [16]u8 = undefined;
     try crypto.pwhash.pbkdf2(&derived_key, password, &salt, rounds, HmacSha256);
 
-    print("Salt: {}\n", .{std.fmt.fmtSliceHexUpper(&salt)});
-    print("PBKDF2 derived key: {}\n", .{std.fmt.fmtSliceHexUpper(&derived_key)});
+    try std.testing.expectEqualSlices(
+        u8,
+        &[_]u8{
+            44,
+            184,
+            223,
+            181,
+            238,
+            128,
+            211,
+            50,
+            149,
+            114,
+            26,
+            86,
+            225,
+            172,
+            116,
+            81,
+        },
+        &derived_key,
+    );
 }
