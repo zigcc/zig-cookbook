4d4091a5773caad560303788c12de4cdbe295cee
jiacai2050 <dev@liujiacai.net>
2024-01-01 15:32:50 +0800

    nitpick refactor

--- src/01-01.zig
+++ src/01-01.zig
@@ -3,27 +3,28 @@ const fs = std.fs;
 const print = std.debug.print;
 
 pub fn main() !void {
-    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .{};
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
     const file = try fs.cwd().openFile("tests/zig-zen.txt", .{});
     defer file.close();
 
-    // Wrap the files reader in a buffered reader.
+    // Wrap the file reader in a buffered reader.
+    // Since it's usually faster to read a bunch of bytes at once.
     var buf_reader = std.io.bufferedReader(file.reader());
     const reader = buf_reader.reader();
 
     var line = std.ArrayList(u8).init(allocator);
     defer line.deinit();
-    const writer = line.writer();
 
+    const writer = line.writer();
     var line_no: usize = 1;
     while (reader.streamUntilDelimiter(writer, '\n', null)) : (line_no += 1) {
-        print("{d}--{s}\n", .{ line_no, line.items });
-
         // Clear the line so we can reuse it.
-        line.clearRetainingCapacity();
+        defer line.clearRetainingCapacity();
+
+        print("{d}--{s}\n", .{ line_no, line.items });
     } else |err| switch (err) {
         error.EndOfStream => {}, // Continue on
         else => |e| return e, // Propagate error
