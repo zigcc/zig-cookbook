9e989ab078976afe33b5d12377bc051bc31ff5c3
Jiacai Liu <dev@liujiacai.net>
2024-01-01 10:22:41 +0800

    refactor: remove unnecessary allocs (#34)

--- src/01-01.zig
+++ src/01-01.zig
@@ -10,12 +10,25 @@ pub fn main() !void {
     const file = try fs.cwd().openFile("tests/zig-zen.txt", .{});
     defer file.close();
 
-    const rdr = file.reader();
+    var rdr = blk: {
+        // Usually buffered reader is faster, so we use one here.
+        var buf_reader = std.io.bufferedReader(file.reader());
+        break :blk buf_reader.reader();
+    };
     var line_no: usize = 0;
-    while (try rdr.readUntilDelimiterOrEofAlloc(allocator, '\n', 4096)) |line| {
-        defer allocator.free(line);
 
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+    var wtr = line.writer();
+    while (true) {
+        // Clear the line so we can reuse it.
+        defer line.clearRetainingCapacity();
+
+        rdr.streamUntilDelimiter(wtr, '\n', 4096) catch |err| switch (err) {
+            error.EndOfStream => return,
+            else => return err,
+        };
         line_no += 1;
-        print("{d}--{s}\n", .{ line_no, line });
+        print("{d}--{s}\n", .{ line_no, line.items });
     }
 }
