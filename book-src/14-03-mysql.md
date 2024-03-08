55997500d5a1895ac881094d80a758a23c528c97
jiacai2050 <dev@liujiacai.net>
2024-03-08 20:00:19 +0800

    fix segfault

--- book-src/14-03-mysql.md
+++ book-src/14-03-mysql.md
@@ -1,6 +1,7 @@
-# Mysql
+# MySQL
 
-As with [previous section](./14-02-sqlite.md), here we introduce [libpq](https://www.postgresql.org/docs/16/libpq-example.html) interface directly, other than [wrapper package](https://github.com/tonis2/zig-postgres).
+
+As with [previous section](./14-01-sqlite.md), here we introduce [libpq](https://www.postgresql.org/docs/16/libpq-example.html) interface directly, other than [wrapper package](https://github.com/tonis2/zig-postgres).
 
 Data models used in demo are the same with the one used in [SQLite section](./14-01-sqlite.md).
 
@@ -8,6 +9,19 @@ Data models used in demo are the same with the one used in [SQLite section](./14
 
 - <https://www.postgresql.org/docs/current/libpq-exec.html#LIBPQ-PQRESULTSTATUS>
 
+
+As with [sqlite section](./14-01-sqlite.md), here we introduce [libmysql_client](https://dev.mysql.com/doc/c-api/8.0/en/c-api-basic-interface-usage.html) interface directly.
+
+Data models used in demo are the same with the one used in [SQLite section](./14-01-sqlite.md).
+
+> Note: After execute a query with `mysql_real_query` like functions, if there are returning results, we must consume the result, Otherwise we will get following error when we execute next query.
+
+```
+Commands out of sync; you can't run this command now
+```
+
+- <https://dev.mysql.com/doc/c-api/8.0/en/mysql-next-result.html>
+
 ```zig
-{{#include ../src/14-02.zig }}
+{{#include ../src/14-03.zig }}
 ```
--- src/14-03.zig
+++ src/14-03.zig
@@ -7,11 +7,9 @@ const Allocator = std.mem.Allocator;
 const c = @cImport({
     @cInclude("mysql.h");
 });
-const exit = std.os.exit;
 const print = std.debug.print;
-const err = std.log.err;
 
-pub const DB_INFO = struct {
+pub const DBInfo = struct {
     host: [:0]const u8,
     user: [:0]const u8,
     password: [:0]const u8,
@@ -23,7 +21,7 @@ pub const DB = struct {
     conn: *c.MYSQL,
     allocator: Allocator,
 
-    fn init(db_info: DB_INFO) !DB {
+    fn init(allocator: Allocator, db_info: DBInfo) !DB {
         const db = c.mysql_init(null);
 
         if (db == null) {
@@ -40,22 +38,24 @@ pub const DB = struct {
             null,
             c.CLIENT_MULTI_STATEMENTS,
         ) == null) {
-            err("Connect to database failed: {s}\n", .{c.mysql_error(db)});
+            print("Connect to database failed: {s}\n", .{c.mysql_error(db)});
             return error.connectError;
         }
 
-        return .{ .conn = db, .allocator = std.heap.page_allocator };
+        return .{
+            .conn = db,
+            .allocator = allocator,
+        };
     }
 
     fn deinit(self: DB) void {
-        _ = c.mysql_close(self.conn);
-
+        c.mysql_close(self.conn);
         return;
     }
 
     fn execute(self: DB, query: []const u8) !void {
         if (c.mysql_real_query(self.conn, query.ptr, query.len) != 0) {
-            err("Exec query failed: {s}\n", .{c.mysql_error(self.conn)});
+            print("Exec query failed: {s}\n", .{c.mysql_error(self.conn)});
             return error.execError;
         }
 
@@ -63,9 +63,6 @@ pub const DB = struct {
     }
 
     fn queryTable(self: DB) !void {
-        const result: *c.MYSQL_RES = c.mysql_store_result(self.conn);
-        defer c.mysql_free_result(result);
-
         const query =
             \\ SELECT
             \\     c.name,
@@ -76,12 +73,13 @@ pub const DB = struct {
             \\
         ;
 
-        if (c.mysql_real_query(self.conn, query, query.len) != 0) {
-            const errorMessage = c.mysql_error(self.conn);
-            print("Query failed: {s}\n", .{errorMessage});
-
-            return error.queryError;
+        try self.execute(query);
+        const result = c.mysql_store_result(self.conn);
+        if (result == null) {
+            print("Store result failed: {s}\n", .{c.mysql_error(self.conn)});
+            return error.storeResultError;
         }
+        defer c.mysql_free_result(result);
 
         while (c.mysql_fetch_row(result)) |row| {
             const cat_name = row[0];
@@ -105,35 +103,37 @@ pub const DB = struct {
 
         const insert_color_stmt: *c.MYSQL_STMT = blk: {
             const stmt = c.mysql_stmt_init(self.conn);
-            const insert_color_query = "INSERT INTO cat_colors (name) values (?)";
-            std.debug.print("stmt is {any}\n", .{stmt.?});
-
-            // errdefer _ = c.mysql_stmt_close(stmt);
-            const result = c.mysql_stmt_prepare(stmt, insert_color_query, insert_color_query.len);
-            if (result != 0) {
-                print("Prepare stmt failed: code:{d}, msg:{s}\n", .{
-                    result,
-                    c.mysql_error(self.conn),
-                });
-
-                return error.prepareStmt;
+            if (stmt == null) {
+                return error.initStmt;
             }
-
-            break :blk stmt.?;
-        };
-
-        const insert_cat_stmt: *c.MYSQL_STMT = blk: {
-            const stmt: ?*c.MYSQL_STMT = c.mysql_stmt_init(self.conn);
-            const insert_cat_query = "INSERT INTO cats (name, color_id) values (?, ?)";
             errdefer _ = c.mysql_stmt_close(stmt);
-            if (c.mysql_stmt_prepare(stmt, insert_cat_query, insert_cat_query.len) != 0) {
-                print("Prepare stmt failed: {s}\n", .{c.mysql_error(self.conn)});
 
+            const insert_color_query = "INSERT INTO cat_colors (name) values (?)";
+            if (c.mysql_stmt_prepare(stmt, insert_color_query, insert_color_query.len) != 0) {
+                print("Prepare color stmt failed, msg:{s}\n", .{c.mysql_error(self.conn)});
                 return error.prepareStmt;
             }
 
             break :blk stmt.?;
         };
+        defer _ = c.mysql_stmt_close(insert_color_stmt);
+
+        const insert_cat_stmt = blk: {
+            const stmt = c.mysql_stmt_init(self.conn);
+            if (stmt == null) {
+                return error.initStmt;
+            }
+            errdefer _ = c.mysql_stmt_close(stmt);
+
+            const insert_cat_query = "INSERT INTO cats (name, color_id) values (?, ?)";
+            if (c.mysql_stmt_prepare(stmt, insert_cat_query, insert_cat_query.len) != 0) {
+                print("Prepare cat stmt failed: {s}\n", .{c.mysql_error(self.conn)});
+                return error.prepareStmt;
+            }
+
+            break :blk stmt.?;
+        };
+        defer _ = c.mysql_stmt_close(insert_cat_stmt);
 
         inline for (cat_colors) |row| {
             const color = row.@"0";
@@ -143,48 +143,35 @@ pub const DB = struct {
             color_binds[0].buffer_type = c.MYSQL_TYPE_STRING;
             color_binds[0].buffer_length = color.len;
             color_binds[0].is_null = 0;
-            color_binds[0].buffer = @constCast(@ptrCast(&color));
+            color_binds[0].buffer = @constCast(@ptrCast(color.ptr));
 
             if (c.mysql_stmt_bind_param(insert_color_stmt, &color_binds)) {
-                print("Bind param failed: {s}\n", .{c.mysql_error(self.conn)});
+                print("Bind color param failed: {s}\n", .{c.mysql_error(self.conn)});
                 return error.bindParamError;
             }
-
-            if (c.mysql_stmt_execute(insert_color_stmt) == 0) {
-                print("Exec stmt failed: {s}\n", .{c.mysql_error(self.conn)});
-
+            if (c.mysql_stmt_execute(insert_color_stmt) != 0) {
+                print("Exec color stmt failed: {s}\n", .{c.mysql_error(self.conn)});
                 return error.execStmtError;
             }
-
+            const last_id = c.mysql_stmt_insert_id(insert_color_stmt);
             _ = c.mysql_stmt_reset(insert_color_stmt);
 
-            const last_id = c.mysql_insert_id(self.conn);
-
             inline for (cat_names) |cat_name| {
-                var bindParams: [*c]c.MYSQL_BIND = @as([*c]c.MYSQL_BIND, @ptrCast(@alignCast(c.malloc(@sizeOf(c.MYSQL_BIND) *% @as(c_ulong, 2)))));
+                var cat_binds = [_]c.MYSQL_BIND{ std.mem.zeroes(c.MYSQL_BIND), std.mem.zeroes(c.MYSQL_BIND) };
+                cat_binds[0].buffer_type = c.MYSQL_TYPE_STRING;
+                cat_binds[0].buffer_length = cat_name.len;
+                cat_binds[0].buffer = @constCast(@ptrCast(cat_name.ptr));
 
-                const cat_name_buf = @as(?*anyopaque, @ptrCast(@as([*c]u8, @ptrCast(@constCast(@alignCast(cat_name))))));
-                const last_id_buf = @as(?*anyopaque, @ptrCast(@as([*c]u8, @ptrCast(@constCast(@alignCast(&last_id))))));
+                cat_binds[1].buffer_type = c.MYSQL_TYPE_LONG;
+                cat_binds[1].length = (@as(c_ulong, 1));
+                cat_binds[1].buffer = @constCast(@ptrCast(&last_id));
 
-                bindParams[0].buffer_type = c.MYSQL_TYPE_STRING;
-                bindParams[0].length = (@as(c_ulong, 1));
-                bindParams[0].is_null = 0;
-                bindParams[0].buffer = cat_name_buf;
-
-                bindParams[1].buffer_type = c.MYSQL_TYPE_STRING;
-                bindParams[1].length = (@as(c_ulong, 1));
-                bindParams[1].is_null = 0;
-                bindParams[1].buffer = last_id_buf;
-
-                if (c.mysql_stmt_bind_param(insert_cat_stmt, bindParams)) {
-                    print("Bind param failed: {s}\n", .{c.mysql_error(self.conn)});
-
+                if (c.mysql_stmt_bind_param(insert_cat_stmt, &cat_binds)) {
+                    print("Bind cat param failed: {s}\n", .{c.mysql_error(self.conn)});
                     return error.bindParamError;
                 }
-
                 if (c.mysql_stmt_execute(insert_cat_stmt) != 0) {
-                    print("Exec stmt failed: {s}\n", .{c.mysql_error(self.conn)});
-
+                    print("Exec cat stmt failed: {s}\n", .{c.mysql_error(self.conn)});
                     return error.execStmtError;
                 }
 
@@ -197,17 +184,21 @@ pub const DB = struct {
 };
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
     const version = c.mysql_get_client_version();
     print("mysql version is {}\n", .{version});
 
-    const info: DB_INFO = .{
+    const info: DBInfo = .{
         .database = "public",
         .host = "127.0.0.1",
         .user = "root",
         .password = "123",
     };
 
-    const db = try DB.init(info);
+    const db = try DB.init(allocator, info);
     defer db.deinit();
 
     try db.execute(
